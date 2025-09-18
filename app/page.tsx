4658b3ace6c40ca3fa822eea5b5b4b92b2c27898
lhaase3 <loha5842@colorado.edu>
2025-09-18 15:14:30 -0600

    redoing directory structure

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+import Hero  from "../components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import Grid from "../components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+import { navItems } from "@/data";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
+
+
+export default function Home() {
+  return (
+   <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+    <div className="max-w-7xl w-full">
+      <FloatingNav navItems={navItems} />
+
+      <Hero />
+      <Grid />
+      <RecentProjects />
+      <Experience />
+      <Footer />
+    </div>
+   </main>
+  );
+}
