0ac701523acb4fe798605dc0cc0d57872da9799c
lhaase3 <loha5842@colorado.edu>
2026-04-11 12:09:52 -0600

    Use PNG logo as site favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,12 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Logan's Portfolio",
-  description: "my next.js portfolio"
+  description: "my next.js portfolio",
+  icons: {
+    icon: "/Sleek%20LH%20logo%20with%20glowing%20orbit.png",
+    shortcut: "/Sleek%20LH%20logo%20with%20glowing%20orbit.png",
+    apple: "/Sleek%20LH%20logo%20with%20glowing%20orbit.png",
+  },
 };
 
 export default function RootLayout({
